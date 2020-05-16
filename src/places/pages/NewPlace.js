e405414456aaba05c1cd697b313f605143cdef4b
sharmavivek223 <sharma.vivek2231999@gmail.com>
2020-05-16 16:22:43 +0530

    added input validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React from "react";
 
+import Input from "../../shared/components/FormElements/Input";
+import {VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH} from "../../shared/util/validators";
+import "./NewPlace.css";
 const NewPlace = () => {
-  return <h2>NewPlace Works!</h2>
+  const titleChangeHandler=(id,value,isValid)=>{
+
+  };
+  const descChangeHandler=(id,value,isValid)=>{
+
+  };
+
+  return (
+    <form className="place-form">
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="*Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="This Field is required"
+        onInput={titleChangeHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="*Description"
+        validators={[VALIDATOR_REQUIRE(),VALIDATOR_MINLENGTH(5)]}
+        errorText="Enter at least 5 character"
+        onInput={descChangeHandler}
+      />
+    </form>
+  );
 };
 
 export default NewPlace;
